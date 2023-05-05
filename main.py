b68b51612318bc56e9a2b5518ae53179cd0f9554
Johannes Zeller <johannes_zeller@sluz.ch>
2023-05-05 11:04:31 +0200

    finish task

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 from bson import ObjectId
+from bson.errors import InvalidId
 from pymongo import MongoClient
 
 
@@ -16,6 +17,29 @@ def ask_user_for_database_selection():
             return client[db_name]
         else:
             print("Database not found, try again...")
+
+
+def ask_user_for_collection_selection():
+    while True:
+        collection_name = input("Select a collection: ")
+        print(db.name + "." + collection_name)
+        if collection_name in collections:
+            return db[collection_name]
+        else:
+            print("Collection not found, try again...")
+
+
+def ask_user_for_document_selection():
+    while True:
+        document_id = input("Select a document: ")
+        print(db.name + "." + collection.name + "." + document_id)
+        try:
+            if collection.count_documents({"_id": ObjectId(document_id)}) > 0:
+                return collection.find_one({"_id": ObjectId(document_id)})
+            else:
+                print("Document not found, try again...")
+        except InvalidId:
+            print("This is an invalid ID, try again...")
 
 
 while True:
@@ -39,10 +63,7 @@ while True:
         show_no_entities_message("Collection")
         continue
 
-    collectionName = input("Select Collection: ")
-    collection = db[collectionName]
-    print(db.name + "." + collectionName)
-
+    collection = ask_user_for_collection_selection()
     print("Documents:")
     documents = collection.find()
     for document in documents:
@@ -51,10 +72,7 @@ while True:
         show_no_entities_message("Document")
         continue
 
-    documentId = input("Select Document: ")
-    document = collection.find_one({"_id": ObjectId(documentId)})
-    print(db.name + "." + collectionName + "." + documentId)
-
+    document = ask_user_for_document_selection()
     for key in document:
         print(key + ": " + str(document[key]))
 
